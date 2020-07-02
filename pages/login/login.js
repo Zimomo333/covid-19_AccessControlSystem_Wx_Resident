32624dc1112b90079df23801176b4173fa8d21e5
Zimomo <lzh120908717@gmail.com>
2020-07-03 02:50:42 +0800

    WxValidate 校验 表单数据
    引入WxValidate.js，
    注意表单信息包裹在data.form内，
    onLoad() 内初始化验证函数 initValidate()

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,63 @@
 // pages/login.js
+import WxValidate from '../../utils/WxValidate.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    form: {   //注意必须在form包裹下，否则equalTo无效
+      username: '',
+      password: ''
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad:function(){
+    this.initValidate();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //报错 
+  showModal(error) {
+    wx.showModal({
+      content: error.msg,
+      showCancel: false,
+    })
+  },
+  //验证函数
+  initValidate() {
+    const rules = {
+      username: {
+        required: true,
+        maxlength: 20
+      },
+      password: {
+        required: true,
+        maxlength: 20
+      }
+    }
+    const messages = {
+      username: {
+        required: '请填写用户名'
+      },
+      password: {
+        required: '请填写密码'
+      }
+    }
+    this.WxValidate = new WxValidate(rules, messages)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    const params = e.detail.value
+    //校验表单
+    if (!this.WxValidate.checkForm(params)) {
+      const error = this.WxValidate.errorList[0]
+      this.showModal(error)
+      return false
+    }
+    this.showModal({
+      msg: '登录成功'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
   }
 })
--- pages/register/register.js
+++ pages/register/register.js
@@ -1,66 +1,99 @@
 // pages/register/register.js
+import WxValidate from '../../utils/WxValidate.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    form: {   //注意必须在form包裹下，否则equalTo无效
+      username: '',
+      password: '',
+      confirm_password: '',
+      sex: '',
+      identity_card: '',
+      house_no: ''
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad:function(){
+    this.initValidate();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  //报错 
+  showModal(error) {
+    wx.showModal({
+      content: error.msg,
+      showCancel: false,
+    })
+  },
+  //验证函数
+  initValidate() {
+    const rules = {
+      username: {
+        required: true,
+        maxlength: 20
+      },
+      password: {
+        required: true,
+        maxlength: 20
+      },
+      confirm_password: {
+        required: true,
+        equalTo: 'password'
+      },
+      sex: {
+        required: true
+      },
+      identity_card: {
+        required: true,
+        idcard: true
+      },
+      house_no: {
+        required: true,
+        maxlength: 15
+      }
+    }
+    const messages = {
+      username: {
+        required: '请填写用户名',
+        maxlength: '用户名最多为 20 位'
+      },
+      password: {
+        required: '请填写密码',
+        maxlength: '密码最多为 20 位'
+      },
+      confirm_password: {
+        required: '请确认密码',
+        equalTo: '输入的两次密码不一致'
+      },
+      sex: {
+        required: '请填写性别'
+      },
+      identity_card: {
+        required: '请填写身份证',
+        idcard: '请输入18位的有效身份证'
+      },
+      house_no: {
+        required: '请填写住址号',
+        maxlength: '住址号最多为 15 位'
+      }
+    }
+    this.WxValidate = new WxValidate(rules, messages)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  formSubmit(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    const params = e.detail.value
+    //校验表单
+    if (!this.WxValidate.checkForm(params)) {
+      const error = this.WxValidate.errorList[0]
+      this.showModal(error)
+      return false
+    }
+    this.showModal({
+      msg: '提交成功'
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  formReset(e) {
+    console.log('form发生了reset事件，携带数据为：', e.detail.value)
   }
 })
